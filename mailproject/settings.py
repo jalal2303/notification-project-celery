bec75dd745976a43c2a108f06c6ffdf24cf14b6e
Jalaludheen <jalaludheen2303@gmail.com>
2025-07-16 16:48:25 +0530

    Fix: use cloud Redis from Railway env

--- mailproject/settings.py
+++ mailproject/settings.py
@@ -105,8 +105,8 @@ TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER")
 # ======================
 # 🔁 Celery Configuration
 # ======================
-CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+CELERY_BROKER_URL = os.getenv("REDIS_URL")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
